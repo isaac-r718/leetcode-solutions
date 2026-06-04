6697d3053a2c4e5fcbe5c50b050fe1a33d62ab7f
Isaac Ríos <riosisaac758@gmail.com>
2026-06-03 21:54:32 -0400

    Time: 6 ms (48%), Space: 19.3 MB (14.9%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,16 @@
+import math
+
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+        
+        divisors_sum = 1
+        
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                divisors_sum += i
+                if i != num // i:
+                    divisors_sum += num // i
+                    
+        return divisors_sum == num
